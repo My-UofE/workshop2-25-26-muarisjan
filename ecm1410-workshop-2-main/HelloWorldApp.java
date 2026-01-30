3ff06d22e6238689ac7c257c2f184bbbfca1860c
muarisjan <muarisjan1@gmail.com>
2026-01-30 01:54:41 +0000

    Adding name and age to some java code

--- ecm1410-workshop-2-main/HelloWorldApp.java
+++ ecm1410-workshop-2-main/HelloWorldApp.java
@@ -1,3 +1,9 @@
 // Add your code below
 
+// HelloWorld application - adding name and age
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World! My name is " + args[0] + " and I am " + args[1]+ " years old"+".");
+    }
+}
 
